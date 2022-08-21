07b5aa6b9df46d4605d2d491048c5564cad76d93
Fatmanur Aycil <fatmanur.aycil@shbndn-mc-5-258-FatmanurAycil.local>
2022-08-21 23:31:10 +0300

    Initial commit

---
+++ src/Emlak.java
@@ -0,0 +1,17 @@
+class Emlak {
+
+    private String baslik;
+    private String aciklama;
+    private String odaSayisi;
+    private int alan;
+    private int kat;
+    private int banyoSayisi;
+    private boolean site;
+    private String sehir;
+    private String ilce;
+    private boolean sahibinden;
+
+
+
+
+}
---
+++ src/Vasita.java
@@ -0,0 +1,28 @@
+class Vasita {
+
+    private String baslik;
+    private String aciklama;
+    private String sehir;
+    private String ilce;
+    private String vitesTipi;
+    private String renk;
+    private boolean kazali;
+    private boolean sahibinden;
+
+    public void Vasita(String baslik, String aciklama, String sehir){
+
+        this.baslik = baslik;
+        this.aciklama= aciklama;
+        this.sehir = sehir;
+
+    }
+
+
+
+
+
+
+
+
+
+}
